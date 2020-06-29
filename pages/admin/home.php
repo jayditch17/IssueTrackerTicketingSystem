73a14a60e59d95708280d9d6b7712cb35dc37395
JAYDITCH BALANSI <2164482@slu.edu.ph>
2020-06-29 12:32:22 +0800

    admin

--- pages/admin/home.php
+++ pages/admin/home.php
@@ -11,10 +11,10 @@
   <title>Issue Tracker</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link href="css/simple-sidebar.css" rel="stylesheet">
+  <link href="../../css/simple-sidebar.css" rel="stylesheet">
 
 </head>
 
@@ -69,11 +69,11 @@
         </div>
       </nav>
 
-      <div class="container-fluid">
+     <!--  <div class="container-fluid">
         <h1 class="mt-4">Simple Sidebar</h1>
         <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
         <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
-      </div>
+      </div> -->
     </div>
     <!-- /#page-content-wrapper -->
 
@@ -81,8 +81,8 @@
   <!-- /#wrapper -->
 
   <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../../vendor/jquery/jquery.min.js"></script>
+  <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Menu Toggle Script -->
   <script>
