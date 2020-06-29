bbe4f059a914e3c85ea8a2f374e26bc4e501c4c7
JAYDITCH BALANSI <2164482@slu.edu.ph>
2020-06-29 16:40:04 +0800

    installed laravel

--- pages/admin/home.php
+++ pages/admin/home.php
@@ -40,7 +40,7 @@
     <div id="page-content-wrapper">
 
       <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-        <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+        <button class="btn btn-primary" id="menu-toggle">Menu</button>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -51,12 +51,12 @@
             <li class="nav-item active">
               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
+           <!--  <li class="nav-item">
               <a class="nav-link" href="#">Link</a>
-            </li>
+            </li> -->
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
+                Account
               </a>
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="#">Action</a>
@@ -69,28 +69,122 @@
         </div>
       </nav>
 
-     <!--  <div class="container-fluid">
-        <h1 class="mt-4">Simple Sidebar</h1>
+       <div class="container-fluid">
+        <!-- <h1 class="mt-4">Simple Sidebar</h1>
         <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
-        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
-      </div> -->
+        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p> -->
+ <table id="selectedColumn" class="table table-sortable" cellspacing="0" width="100%">
+  <thead>
+    <tr>
+      <th>#
+      </th>
+      <th>Project
+      </th>
+      <th>Tracker
+      </th>
+      <th>Status
+      </th>
+      <th>Priority
+      </th>
+      <th>Subject
+      </th>
+      <th>Assinee
+      </th>
+      <th>Updated
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+<tr>
+        <td>1</td>
+        <td>Dom</td>
+        <td>35</td>
+        <td>Web Developer</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Rebecca</td>
+        <td>29</td>
+        <td>Teacher</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>John</td>
+        <td>30</td>
+        <td>Civil Engineer</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>Andre</td>
+        <td>20</td>
+        <td>Dentist</td>
+      </tr>
+  </tbody>
+
+</table>
+      </div> 
+
     </div>
     <!-- /#page-content-wrapper -->
 
   </div>
   <!-- /#wrapper -->
+ <script>
 
-  <!-- Bootstrap core JavaScript -->
-  <script src="../../vendor/jquery/jquery.min.js"></script>
-  <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    @param {HTMLTableElement}
+    @param{number}
+    @param{boolean}
 
-  <!-- Menu Toggle Script -->
-  <script>
+    function sortTableByColumn(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
+
+    // Sort each row
+    const sortedRows = rows.sort((a, b) => {
+        const aColText = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+        const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+
+        return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+    });
+
+    // Remove all existing TRs from the table
+    while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+    }
+
+    // Re-add the newly sorted rows
+    tBody.append(...sortedRows);
+
+    // Remember how the column is currently sorted
+    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-desc", !asc);
+}
+
+document.querySelectorAll(".table-sortable th").forEach(headerCell => {
+    headerCell.addEventListener("click", () => {
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+    });
+});
+
     $("#menu-toggle").click(function(e) {
       e.preventDefault();
       $("#wrapper").toggleClass("toggled");
     });
+
   </script>
+  <!-- Bootstrap core JavaScript -->
+  <script src="../../vendor/jquery/jquery.min.js"></script>
+  <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+  <!-- Menu Toggle Script -->
+ 
 
 </body>
 
