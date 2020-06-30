a82ffc28461bff52b73e93deeda95125e10b4c27
JAYDITCH BALANSI <2164482@slu.edu.ph>
2020-06-30 16:52:49 +0800

    new laravel

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+
+
+Route::get('/admin-home', function () {
+    return view('subview.pages.admin.home');
+});
+
+Route::get('/admin-newTicket', function () {
+    return view('subview.pages.admin.newTicket');
+});
+
+Route::get('/admin-viewTicket', function () {
+    return view('subview.pages.admin.viewTickets');
 });
